8afa4d582fbe2667130fd408daefce33f913cf7d
Marius Løken Skauger <mariskau@stud.ntnu.no>
2023-06-18 00:12:30 +0200

    tranparent signcolumn

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,6 +5,7 @@ function SetColorsAndBackground(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
 end
 
 SetColorsAndBackground()
