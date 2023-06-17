a40d45db5b385f25530518cbecacee953a7d36fd
Marius Løken Skauger <mariskau@stud.ntnu.no>
2023-06-17 22:28:21 +0200

    chaning package manager

---
+++ after/plugin/colors.lua
@@ -0,0 +1,10 @@
+-- Set default colorscheme or gruvbox. Tranparent background
+function SetColorsAndBackground(color)
+	color = color or "gruvbox"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+SetColorsAndBackground()
--- lua/marius/init.lua
+++ lua/marius/init.lua
@@ -1,2 +1,1 @@
 require("marius.remap")
-require("marius.plug")
