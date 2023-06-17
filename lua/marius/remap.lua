4c2e152d95ce3ecfbcfeaaffc0cab96654c9dbca
Marius Løken Skauger <mariskau@stud.ntnu.no>
2023-06-18 00:34:49 +0200

    keymap

--- lua/marius/remap.lua
+++ lua/marius/remap.lua
@@ -8,3 +8,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Keep cursor in middle for half page jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- replace current word
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
